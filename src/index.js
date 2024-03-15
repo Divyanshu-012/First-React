639a671fcbc4bcb66207234c9c0a2d959ad06580
divyanshu-012 <kapariyadiv@gmailcom>
2024-03-15 16:22:14 +0530

    First commit of React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+// function Greeting() {
+//     return <h2>My First Component</h2>;
+
+// }
+
+function BookList() {
+    return (
+        <section className='booklist'>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+
+
+
+        </section>
+    )
+
+
+}
+
+const Book = () => {
+    return (
+        <article className='book'>
+            <Image />
+            <Title />
+            <Author />
+
+        </article>
+    );
+};
+
+const Image = () => (
+    <img
+        src='https://images-na.ssl-images-amazon.com/images/I/81AHTyq2wVL._AC_UL300_SR300,200_.jpg'
+        alt='The HouseMaid'
+    />
+);
+const Title = () => <h2> The HouseMaid</h2>;
+const Author = () => {
+    return <h4>Freida McFadden</h4>;
+
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
+
+
+
